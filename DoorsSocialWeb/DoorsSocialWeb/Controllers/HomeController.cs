86a37a0539730e58c2feb4a995f470dac91f08f7
Dagur Leo <dagurl14@ru.is>
2015-05-06 15:46:45 +0000

    tester

--- DoorsSocialWeb/DoorsSocialWeb/Controllers/HomeController.cs
+++ DoorsSocialWeb/DoorsSocialWeb/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace DoorsSocialWeb.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            return View("mamma");
         }
 
         public ActionResult About()
