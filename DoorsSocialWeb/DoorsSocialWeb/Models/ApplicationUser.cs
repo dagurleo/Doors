0db757c8dba73d9d3d84c000f4de839aab0e3849
Dagur Leo <dagurl14@ru.is>
2015-05-07 20:02:23 +0000

    moved applicationuser

---
+++ DoorsSocialWeb/DoorsSocialWeb/Models/ApplicationUser.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace DoorsSocialWeb.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string registerEmail { get; set; }
+        public string displayName { get; set; }
+        public string displayImageUrl { get; set; }
+        public string displayAbout { get; set; }
+        public string displayEmail { get; set; }
+        public string displayPhoneNumber { get; set; }
+        public bool userIsModerator { get; set; }
+        public bool userIsAdministrator { get; set; }
+    }
+}
