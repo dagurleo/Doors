d6619497bd4a75cbf95f03f7228710cb823d582b
arnarf14 <arnarf14@ru.is>
2015-05-09 15:13:10 +0000

    fixed post repo/service

--- DoorsSocialWeb/DoorsSocialWeb/Repositories/PostRepository.cs
+++ DoorsSocialWeb/DoorsSocialWeb/Repositories/PostRepository.cs
@@ -17,7 +17,9 @@ namespace DoorsSocialWeb.Repositories
          */
         public IEnumerable<Post> getPostByID(int postID)
         {
-            var queryAllPostsMadeByPostID = (from post in db.Posts where post.ID == postID select post);
+            var queryAllPostsMadeByPostID = (from post in db.Posts
+                                             where post.ID == postID
+                                             select post);
             return queryAllPostsMadeByPostID;
         }
 
@@ -32,50 +34,105 @@ namespace DoorsSocialWeb.Repositories
 
         /*
          * Returns all image posts, for newsfeed filtering.
-         * Need to add to table postIsImage bool variable, to be able to filter.
-         *
+         */
         public IEnumerable<Post> getAllImagePosts()
         {
-            var queryAllImagePosts = (from post in db.Posts where post.)
+            var queryAllImagePosts = (from post in db.Posts
+                                      where post.postIsImage == true
+                                      select post);
+            return queryAllImagePosts;
         }
-         */
+
 
         /*
          * Returns all text posts, for newsfeed filtering
-         * same as above, or postisimage bool == false; problem if we want users to be able to add f.x. videos in the future.... what then ?
-         *
+         */
         public IEnumerable<Post> getAllTextPosts()
         {
-            //var queryAllTextPosts = (from post in db.Posts where post.postistext == true);
-            IEnumerable<Post> bla;
-            return bla;
+            var queryAllTextPosts = (from post in db.Posts 
+                                     where post.postIsImage == false
+                                     select post);
+            return queryAllTextPosts;
         }
         
-        *
+        /*
          * Returns all posts with that userID
-         *
+         */
         public IEnumerable<Post> getAllPostByID(string userID)
         {
-            var queryAllPostsMadeByUser = (from post in db.Posts where post.authorID == userID select post);
+            var queryAllPostsMadeByUser = (from post in db.Posts
+                                           where post.authorID == userID
+                                           select post);
             return queryAllPostsMadeByUser;
         }
 
-        *
+        /*
          * Return all image post with that userID
-         * SAME AS ABOVE
-         *
+         */
         public IEnumerable<Post> getAllImagePostsByID(string userID)
         {
-            
+            var queryAllImagePostsMadeByUser = (from post in db.Posts
+                                                where post.authorID == userID
+                                                && post.postIsImage == true
+                                                select post);
+            return queryAllImagePostsMadeByUser;
         }
 
-        *
+        /*
          * Return all text posts with that userID
          * SAME AS ABOVE
-         *
+         */
         public IEnumerable<Post> getAllTextPostsByID(string userID)
         {
+            var queryAllImagePostsMadeByUser = (from post in db.Posts
+                                                where post.authorID == userID
+                                                && post.postIsImage == false
+                                                select post);
+            return queryAllImagePostsMadeByUser;
+        }
 
+        public ApplicationUser getUserOfPost(int postID)
+        {
+            var user = (from post in db.Posts
+                        where post.ID == postID
+                        join u in db.Users
+                        on post.authorID equals u.Id
+                        select u).Single();
+            return user;
+        }
+
+        public IEnumerable<Post> getAllGroupPostsByGroupID(int groupID)
+        {
+            var allGroupPosts = (from post in db.Posts
+                                 where post.postIsInGroup == true &&
+                                 post.groupId == groupID
+                                 orderby post.dateCreated ascending
+                                 select post);
+            return allGroupPosts;
+        }
+
+        public IEnumerable<Post> getGroupTopicPostsByGroupID(int groupID, int topicID)
+        {
+            var groupTopicPosts = (from post in db.Posts
+                                   where post.postIsInGroup == true &&
+                                   post.groupId == groupID &&
+                                   post.groupTopicID == topicID
+                                   orderby post.dateCreated ascending
+                                   select post
+                                   );
+            return groupTopicPosts;
+        }
+
+        public Group getGroupByPostID(int postID)
+        {
+            var group = (from post in db.Posts
+                         where post.postIsInGroup == true &&
+                         post.ID == postID
+                         join gr in db.Groups
+                         on post.groupId equals gr.ID
+                         select gr
+                         ).Single();
+            return group;
         }
 
         /*
--- DoorsSocialWeb/DoorsSocialWeb/Services/PostService.cs
+++ DoorsSocialWeb/DoorsSocialWeb/Services/PostService.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using DoorsSocialWeb.Repositories;
 using DoorsSocialWeb.Models.EntityModels;
+using DoorsSocialWeb.Models;
 
 namespace DoorsSocialWeb.Services
 {
@@ -11,23 +12,18 @@ namespace DoorsSocialWeb.Services
     {
         private PostRepository postRepo = new PostRepository();
 
-        /*
-         * Return a post with that ID
-        */
-        public List<Post> getPostByID(int postID)
+        
+        public IEnumerable<Post> getPostByID(int postID)
         {
             return postRepo.getPostByID(postID).ToList();
         }
 
-        /*
-         * Returns all posts, for newsfeed view.
-         */
-        public List<Post> getAllPosts()
+        public IEnumerable<Post> getAllPosts()
         {
             return postRepo.getAllPosts().ToList();
         }
-/*
-        public List<Post> getAllImagePosts()
+
+        public IEnumerable<Post> getAllImagePosts()
         {
             return postRepo.getAllImagePosts().ToList();
         }
@@ -36,36 +32,48 @@ namespace DoorsSocialWeb.Services
         {
             return postRepo.getAllTextPosts().ToList();
         }
-*/
-        /*
-         * Returns all posts with that userID
-         *
-        public List<Post> getAllPostByID(string userID)
+
+        public IEnumerable<Post> getAllPostByID(string userID)
         {
             return postRepo.getAllPostByID(userID).ToList();
         }
 
-        *
-         * Return all image post with that userID
-         * SAME AS ABOVE
-         *
-        public List<Post> getAllImagePostsByID(string userID)
+        public IEnumerable<Post> getAllImagePostsByID(string userID)
         {
             return postRepo.getAllImagePostsByID(userID).ToList();
         }
 
-        *
-         * Return all text posts with that userID
-         * SAME AS ABOVE
-         *
-        public List<Post> getAllTextPostsByID(string userID)
+        public IEnumerable<Post> getAllTextPostsByID(string userID)
         {
             return postRepo.getAllTextPostsByID(userID).ToList();
         }
 
-        *
-         * Create a new post, need info on variables to parse to repo..
-         */
+        public IEnumerable<Post> getAllGroupPostsByGroupID(int groupID)
+        {
+            return postRepo.getAllGroupPostsByGroupID(groupID);
+        }
+
+        public IEnumerable<Post> getGroupTopicPostsByGroupID(int groupID, int topicID)
+        {
+            return postRepo.getGroupTopicPostsByGroupID(groupID, topicID);
+        }
+
+        public ApplicationUser getUserOfPost(int postID)
+        {
+            return postRepo.getUserOfPost(postID);
+        }
+
+        public Group getGroupByPostID(int postID)
+        {
+            return postRepo.getGroupByPostID(postID);
+        }
+       
+
+
+
+
+
+
         public void addNewPost()
         {
             postRepo.addNewPost();
