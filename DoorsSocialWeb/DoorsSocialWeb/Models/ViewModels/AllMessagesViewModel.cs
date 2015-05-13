16fb663f225f9a8d00e5ff0d99e2da29ebcb667a
dagurleo <dagurl14@ru.is>
2015-05-13 15:42:19 +0000

    woops

--- DoorsSocialWeb/DoorsSocialWeb/Models/ViewModels/AllMessagesViewModel.cs
+++ DoorsSocialWeb/DoorsSocialWeb/Models/ViewModels/AllMessagesViewModel.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using DoorsSocialWeb.Models.EntityModels;
 
 namespace DoorsSocialWeb.Models.ViewModels
 {
-    public class AllMessagesViewModel
+    public class AllMessagesViewModel : LoggedInSharedLayoutViewModel
     {
+        public IEnumerable<ApplicationUser> messageUsers { get; set; }
+        public IEnumerable<Message> firstMessages { get; set; } 
+        
     }
 }
--- DoorsSocialWeb/DoorsSocialWeb/Models/ViewModels/MessageViewModel.cs
+++ DoorsSocialWeb/DoorsSocialWeb/Models/ViewModels/MessageViewModel.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using DoorsSocialWeb.Models.EntityModels;
 
 namespace DoorsSocialWeb.Models.ViewModels
 {
-    public class MessageViewModel
+    public class MessageViewModel : LoggedInSharedLayoutViewModel
     {
+        public IEnumerable<Message> messagesWithUser { get; set; }
     }
 }
