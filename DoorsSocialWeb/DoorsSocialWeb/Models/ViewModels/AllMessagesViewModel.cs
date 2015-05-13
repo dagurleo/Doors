e5c70a57301c54675f644927a171bafaef87d784
dagurleo <dagurl14@ru.is>
2015-05-13 15:34:26 +0000

    added message views and viewmodels

---
+++ DoorsSocialWeb/DoorsSocialWeb/Models/ViewModels/AllMessagesViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DoorsSocialWeb.Models.ViewModels
+{
+    public class AllMessagesViewModel
+    {
+    }
+}
