50278b10c6653a2e430381a6c8d6ed3e3a3fad19
arnarf14 <arnarf14@ru.is>
2015-05-14 23:32:42 +0000

    Topics working

--- DoorsSocialWeb/DoorsSocialWeb/Models/ViewModels/TopicViewModel.cs
+++ DoorsSocialWeb/DoorsSocialWeb/Models/ViewModels/TopicViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using DoorsSocialWeb.Services;
 using DoorsSocialWeb.Models.EntityModels;
 
 namespace DoorsSocialWeb.Models.ViewModels
@@ -9,8 +10,15 @@ namespace DoorsSocialWeb.Models.ViewModels
     public class TopicViewModel : LoggedInSharedLayoutViewModel
     {        
         public Group currentGroup { get; set; }
+        public Topic currentTopic { get; set; }
+        public UserService userService = new UserService();
         public IEnumerable<Topic> topics { get; set; }
         public IEnumerable<Post> posts { get; set; }
+
+        public IEnumerable<ApplicationUser> getGroupMembersByGroupId(int groupID)
+        {
+            return userService.getMembersOfCurrentGroup(groupID);
+        }
 
     }
 }
